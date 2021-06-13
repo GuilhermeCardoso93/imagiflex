5e3d85a8d81f4477e59f66f92a24cb2c9ffb2dd6
Guilherme Cardoso da Silva <guilhermecardosowork@gmail.com>
2021-06-13 16:18:12 -0300

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import CONST from './data/contants';
+import CONST from "./data/constants";
 
-import Hero from './components/Hero';
-import NavBar from './components/NavBar';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Loading from "./components/Loading";
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   const { URL, APISTRING } = CONST;
 
-  const [movies, setMovies] = useState();
-  const [series, setSeries] = useState();
+  const [movies, setMovies] = useState({
+    results: [],
+  });
+  const [series, setSeries] = useState({
+    results: [],
+  });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +35,8 @@ const App = () => {
       );
       const seriesData = await series.json();
       setSeries(seriesData);
+
+      setLoading(false);
     };
 
     fetchData();
@@ -45,14 +53,25 @@ const App = () => {
     }
     return [];
   };
-
   return (
-    <div className='m-auto antialised font-sans bg-black text-white'>
-      <Hero {...getFeaturedMovie()} />
-      <NavBar />
-      <Carousel title='Filmes Populares' data={getMovieList()} />
-      <Carousel title='Séries Populares' data={series?.results} />
-      <Carousel title='Placeholder' />
+    <div className="m-auto antialised font-sans bg-black text-white">
+      {loading && (
+        <>
+          <Loading />
+          <NavBar />
+        </>
+      )}
+      {
+        !loading &&(
+          <>
+            <Hero {...getFeaturedMovie()} />
+            <NavBar />
+            <Carousel title="Filmes Populares" data={getMovieList()} />
+            <Carousel title="Séries Populares" data={series?.results} />
+            <Carousel title="Placeholder" />
+          </>
+        )
+      }
       <Footer />
     </div>
   );
--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,29 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import CONST from '../data/contants';
+import CONST from '../data/constants';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlayCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
-import "./Poster.css";
-import Score from "./Score";
-import { Movie } from "./../data/mock";
+import Score from './Score';
+
+import { Movie } from './../data/mock';
+
+import './Poster.css';
 
 const Poster = ({ cover, poster_path, title, name, vote_average }: Movie, index: number) => {
   const { IMAGEURL } = CONST;
-  return (
-  <article
-    className="relative transition-all duration-500 ease-in-out transform hover:scale-110"
-    key={index}
-  >
-    <img src={poster_path ? `${IMAGEURL}/w200/${poster_path}`: cover} className="max-w-md max-h-62 px-0.5" alt={title ? title : name} />
-    <div className="poster cursor-pointer absolute inset-0 w-full h-full px-4 py-8 grid place-items-center text-center bg-black leading-6 bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
-    <h2 className="text-2xl">{title ? title : name }</h2>
+    return (
+    <article className="relative transition-all duration-500 ease-in-out transform hover:scale-110" key={index}>
+      <img src={poster_path ? `${IMAGEURL}/w200/${poster_path}` : cover} alt={title ? title : name} />
+      <div className="poster cursor-pointer absolute inset-0 w-full h-full px-4 py-8 grid place-items-center text-center leading-6 bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
+          <h2 className="text-2xl">{title ? title : name }</h2>
           <FontAwesomeIcon icon={faPlayCircle} size="5x" />
           <Score value={vote_average} />
-    </div>
-  </article>
-)
+      </div>
+    </article>
+  )
 };
 
 export default Poster;
